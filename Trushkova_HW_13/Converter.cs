0672860307a5f5641060a2b2b04f3b7576cde678
JuliaTrushkova <kosyuska@yandex.ru>
2022-01-19 00:05:51 +0300

    Добавьте файлы проекта.

---
+++ Trushkova_HW_13/Converter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trushkova_HW_13
+{
+    internal class Converter
+    {
+        double _usdToGrivna;
+        double _eurToGrivna;
+        double _rubToGrivna;
+        public Converter(double usd, double eur, double rub)
+        {
+            this._usdToGrivna = usd;
+            this._eurToGrivna = eur;
+            this._rubToGrivna = rub;
+        }
+
+        public double UsdToGrivna(double usd)
+            => usd * _usdToGrivna;
+
+        public double EurToGrivna(double eur)
+            => eur * _eurToGrivna;
+
+        public double RubToGrivna(double rub)
+            => rub * _rubToGrivna;
+
+
+        public double GrivnaToUsd(double grivna)
+        {
+            if (_usdToGrivna != 0)
+            {
+                return grivna / _usdToGrivna;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public double GrivnaToEur(double grivna)
+        {
+            if (_eurToGrivna != 0)
+            {
+                return grivna / _eurToGrivna;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public double GrivnaToRub(double grivna)
+        {
+            if (_rubToGrivna != 0)
+            {
+                return grivna / _rubToGrivna;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Trushkova_HW_13/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Trushkova_HW_13;
+
+Console.WriteLine("Hello, Teacher!");
+
+double euroToGrivna = 32.05d;
+double usdToGrivna = 28.25d;
+double rubToGrivna = 0.355d;
+Converter converter = new Converter(usdToGrivna, euroToGrivna, rubToGrivna);
+
+double usd = 100;
+Console.WriteLine($"{usd} долларов равно {converter.UsdToGrivna(usd)} гривен");
+
+double euro = 100;
+Console.WriteLine($"{euro} евро равно {converter.EurToGrivna(euro)} гривен");
+
+double rub = 100;
+Console.WriteLine($"{rub} рублей равно {converter.RubToGrivna(rub)} гривен");
+
+double grivna = 100;
+Console.WriteLine($"{grivna} гривен равно {converter.GrivnaToUsd(grivna)} долларов, " +
+    $"{converter.GrivnaToEur(grivna)} евро и {converter.GrivnaToRub(grivna)} рублей");
