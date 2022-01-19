6f6c9059967f341b887daa801762b69f8b707263
Julia Trushkova <kosyuska@yandex.ru>
2022-01-19 22:50:15 +0300

    feat: add Task6
    user

---
+++ Task6/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Task6;
+
+Console.WriteLine("Hello, Teacher!");
+
+User user = new User("Julyalya", new DateTime());
+user.Name = "Julia";
+user.Age = 32;
+user.Surname = "Lol";
+
+Console.WriteLine($"Пользователь {user.Login} c именем {user.Name} " +
+    $"и фамилией {user.Surname} возраст {user.Age} зарегистрировался {user.DateTimeLogin}");
+
---
+++ Task6/User.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    internal class User
+    {
+        string _login;
+        string _name;
+        string _surname;
+        int _age;
+        readonly DateTime _date;
+
+        public User(DateTime dateTime)
+        {
+            _date = dateTime;
+        }
+
+        public User(string login, DateTime dateTime) : this(dateTime)
+        {
+            this._login = login;
+        }
+
+        public string Login
+        {
+            get
+            {
+                if (_login != null)
+                {
+                    return _login;
+                }
+                else
+                {
+                    return "Логин не задан";
+                }
+            }
+            set { _login = value; }
+        }
+
+        public string Name
+        {
+            get
+            {
+                if (_name != null)
+                {
+                    return _name;
+                }
+                else
+                {
+                    return "Имя не задано";
+                }
+            }
+            set { _name = value; }
+        }
+
+        public string Surname
+        {
+            get
+            {
+                if (_surname != null)
+                {
+                    return _surname;
+                }
+                else
+                {
+                    return "Фамилия не задана";
+                }
+            }
+            set { _surname = value; }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        public DateTime DateTimeLogin
+        {
+            get { return _date; }
+        }
+    }
+}
