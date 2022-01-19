c88df94da1e74ec60a0f94458c0ec104ddbdb030
Julia Trushkova <kosyuska@yandex.ru>
2022-01-19 21:20:40 +0300

    feat: finish Task3
    calculate salary and tax

--- Task3/Employee.cs
+++ Task3/Employee.cs
@@ -11,16 +11,122 @@ namespace Task3
         string _name;
         string _surname;
         string _position;
-        string _experience;
+        int _experience;
+        double _salary;
 
 
-        public Employee (string name, string surname)
+        public Employee(string name, string surname)
         {
             this._name = name;
             this._surname = surname;
         }
 
+        public string Name
+        {
+            get
+            {
+                if (_name != null)
+                {
+                    return _name;
+                }
+                else
+                {
+                    return "Имя сотрудника не задано";
+                }
+            }
+            set { _name = value; }
+        }
 
-            
+        public string Surname
+        {
+            get
+            {
+                if (_surname != null)
+                {
+                    return _surname;
+                }
+                else
+                {
+                    return "Фамилия сотрудника не задана";
+                }
+            }
+            set { _surname = value; }
+        }
+
+        public string Position
+        {
+            get
+            {
+                if (_position != null)
+                {
+                    return _position;
+                }
+                else
+                {
+                    return "Название должности не задано";
+                }
+            }
+            set { _position = value; }
+        }
+
+        public int Experience
+        { get { return _experience; } set { _experience = value; } }
+
+        public double Salary
+        {
+            get
+            {
+                CalculateSalary();
+                return _salary;
+            }
+            set { _salary = value; }
+        }
+
+        void CalculateSalary()
+        {
+            _salary = 0;
+
+            switch (_position)
+            {
+                case "директор":
+                    _salary = 200000;
+                    break;
+                case "руководитель":
+                    _salary = 100000;
+                    break;
+                case "рядовой сотрудник":
+                    _salary = 50000;
+                    break;
+            }
+
+            if (_experience < 5)
+            {
+                _salary *= 1;
+            }
+            else if ((_experience >= 5) && (_experience < 10))
+            {
+                _salary *= 1.2;
+            }
+            else if ((_experience >= 10) && (_experience < 15))
+            {
+                _salary *= 1.4;
+            }
+            else if ((_experience >= 15) && (_experience < 20))
+            {
+                _salary *= 1.6;
+            }
+            else if (_experience >= 20)
+            {
+                _salary *= 2;
+            }
+        }
+
+        public double CalculateTaxCollection(int taxProcent)
+        {
+            double tax;
+            tax = _salary * taxProcent / 100;
+            return tax;
+        }
+
     }
 }
--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Task3;
+
 Console.WriteLine("Hello, World!");
+
+Employee employee = new Employee("Антон", "Иванов");
+employee.Position = "руководитель";
+employee.Experience = 12;
+
+int taxProcent = 35; //в процентах
+
+Console.WriteLine($"Сотрудник {employee.Name} {employee.Surname} " +
+    $"имеет должность {employee.Position} и зарплату {employee.Salary} рублей. \n" +
+    $"Налоговый сбор для него составляет {employee.CalculateTaxCollection(taxProcent)} рублей");
