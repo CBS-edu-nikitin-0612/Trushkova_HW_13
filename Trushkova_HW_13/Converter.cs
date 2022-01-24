235f9b16849cec10f0c86fb1d159a4cad15175de
Nikita ZIBOROV <Nikita.ZIBOROV@livenau.net>
2022-01-24 14:08:23 +0200

    Все сделали круто )

--- Trushkova_HW_13/Converter.cs
+++ Trushkova_HW_13/Converter.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Trushkova_HW_13
+﻿namespace Trushkova_HW_13
 {
     internal class Converter
     {
